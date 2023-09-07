6f56a4d9cfaa1fa8d37430218543bda4edf73479
Blake Rutledge <blakerutledge@users.noreply.github.com>
2023-09-07 14:52:43 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -82,11 +82,15 @@ const staticFileExtensions = [
 	'zip'
 ];
 
+const sleep = async ( delay ) => {
+	await new Promise( resolve => setTimeout( resolve, delay ) )
+}
+
 const pupperender = async (url, timeout) => {
 	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
 	await page.goto(url + '?bot_mode', {waitUntil: 'networkidle0'});
-	await page.waitFor(timeout);
+	await sleep( 3000 );
 	const content = await page.content();
 	await browser.close();
 	return content;
